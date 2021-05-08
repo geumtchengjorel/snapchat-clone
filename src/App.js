8cc293375f6eb759ffb4a509153bca037410aebc
Aymar GEUMTCHENG <geumtchengjorel@yahoo.fr>
2021-05-08 14:04:05 +0200

    Add of a frame of a phone for the snapchat application

--- src/App.js
+++ src/App.js
@@ -1,68 +1,73 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import WebcamCapture from './WebcamCapture';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Preview from './Preview';
-import Chats from './Chats';
-import ChatView from './ChatView';
-import { useDispatch, useSelector } from 'react-redux';
-import { login, logout, selectUser } from './features/appSlice';
-import Login from './Login';
-import { auth } from './firebase';
+import React, { useEffect } from "react";
+import "./App.css";
+import WebcamCapture from "./WebcamCapture";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Preview from "./Preview";
+import Chats from "./Chats";
+import ChatView from "./ChatView";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/appSlice";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged((authUser) => {
-          if (authUser) {
-              dispatch(login({
-                  username: authUser.displayName,
-                  profilePic: authUser.photoURL,
-                  id: authUser.uid,
-              }));
-          }
-          else {
-              dispatch(logout());
-          }
-      });
-      return unsubscribe;
-      
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="app">
-       <Router>
-         {!user ? (
-           <Login />
-         ): (
-           <div className="app__body">
-            <Switch>
-              <Route path="/test">
-                <h1>Yeah Man</h1>
-              </Route>
-              <Route exact path="/">
-                <WebcamCapture />
-              </Route>
-              <Route exact path="/preview">
-                <Preview />
-              </Route>
-              <Route exact path="/chats">
-                <Chats />
-              </Route>
-              <Route path="/chats/view">
-                <ChatView />
-              </Route>
-            </Switch>
-          </div>
-         )}
-          
-        </Router>
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <img
+              className="app__logo"
+              src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg"
+              alt=""
+            />
+            <div className="app__body">
+              <div className="app__bodyBackground">
+                <Switch>
+                  <Route path="/test">
+                    <h1>Yeah Man</h1>
+                  </Route>
+                  <Route exact path="/">
+                    <WebcamCapture />
+                  </Route>
+                  <Route exact path="/preview">
+                    <Preview />
+                  </Route>
+                  <Route exact path="/chats">
+                    <Chats />
+                  </Route>
+                  <Route path="/chats/view">
+                    <ChatView />
+                  </Route>
+                </Switch>
+              </div>
+            </div>
+          </>
+        )}
+      </Router>
     </div>
   );
 }
